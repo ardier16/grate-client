4ac7196b19904a2d14366af9fd31d5fbb8cef6cb
ardier16 <ardier16@gmail.com>
2019-04-22 08:41:07 +0300

    Add sign up form

--- src/validators.js
+++ src/validators.js
@@ -1,1 +1,5 @@
+import * as validators from 'vuelidate/lib/validators'
+
+export const password = value => validators.minLength(6)(value)
+
 export * from 'vuelidate/lib/validators'
--- src/vue/forms/sign-up-form.vue
+++ src/vue/forms/sign-up-form.vue
@@ -1,8 +1,135 @@
 <template>
-  <div />
+  <form class="app-form auth-form" @submit.prevent="submit">
+    <div class="app__form-row">
+      <div class="app__form-field">
+        <input-field
+          v-model="form.email"
+          @blur="touchField('form.email')"
+          name="sign-up-email"
+          :label="'auth-pages.email' | globalize"
+          :error-message="getFieldErrorMessage('form.email')"
+        />
+      </div>
+    </div>
+
+    <div class="app__form-row">
+      <div class="app__form-field">
+        <input-field
+          v-model="form.username"
+          @blur="touchField('form.username')"
+          name="sign-up-username"
+          :label="'auth-pages.username' | globalize"
+          :error-message="getFieldErrorMessage('form.username')"
+        />
+      </div>
+    </div>
+
+    <div class="app__form-row">
+      <div class="app__form-field">
+        <input-field
+          v-model="form.password"
+          @blur="touchField('form.password')"
+          name="sign-up-password"
+          type="password"
+          :error-message="getFieldErrorMessage('form.password')"
+          :label="'auth-pages.password' | globalize"
+        />
+      </div>
+    </div>
+
+    <div class="app__form-row">
+      <div class="app__form-field">
+        <input-field
+          v-model="form.confirmPassword"
+          @blur="touchField('form.confirmPassword')"
+          id="sign-up-confirm-password"
+          type="password"
+          :error-message="getFieldErrorMessage('form.confirmPassword')"
+          :label="'auth-pages.confirm-password' | globalize"
+        />
+      </div>
+    </div>
+
+    <div class="app__form-actions">
+      <button
+        type="submit"
+        class="auth-form__submit-btn"
+        :disabled="formMixin.isDisabled"
+      >
+        {{ 'auth-pages.sign-up' | globalize }}
+      </button>
+    </div>
+  </form>
 </template>
 
 <script>
+import FormMixin from '@/vue/mixins/form.mixin'
+
+import {
+  email,
+  required,
+  password,
+  sameAs,
+} from '@validators'
+
+import { vuexTypes } from '@/vuex'
+import { mapActions } from 'vuex'
+
 export default {
+  name: 'sign-up-form',
+  mixins: [FormMixin],
+
+  data: _ => ({
+    form: {
+      email: '',
+      username: '',
+      password: '',
+      confirmPassword: '',
+    },
+  }),
+
+  validations: {
+    form: {
+      email: { required, email },
+      username: { required },
+      password: { required, password },
+      confirmPassword: {
+        required,
+        password,
+        sameAsPassword: sameAs(function () { return this.form.password }),
+      },
+    },
+  },
+
+  methods: {
+    ...mapActions({
+      signUp: vuexTypes.SIGN_UP,
+    }),
+
+    async submit () {
+      if (!this.isFormValid()) {
+        return
+      }
+
+      this.disableForm()
+      try {
+        await this.signUp({
+          email: this.form.email.toLowerCase(),
+          name: this.form.username.toLowerCase(),
+          password: this.form.password,
+        })
+
+        this.$router.push({ name: 'app' })
+      } catch (e) {
+        alert(e.message)
+      }
+      this.enableForm()
+    },
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+@import './app-form';
+@import './auth-form';
+</style>
