645cf50d5c3c4635093c890eeaa55e2f82966e9a
ardier16 <ardier16@gmail.com>
2019-04-22 22:44:44 +0300

    Add profile form

---
+++ src/vue/forms/profile-form.vue
@@ -0,0 +1,93 @@
+<template>
+  <form class="app-form profile-form" @submit.prevent="submit">
+    <div class="app__form-row">
+      <div class="app__form-field">
+        <input-field
+          v-model="form.name"
+          name="profile-name"
+          :label="'profile-form.name' | globalize"
+        />
+      </div>
+    </div>
+
+    <div class="app__form-row">
+      <div class="app__form-field">
+        <input-field
+          v-model="form.birthDate"
+          name="profile-birth-date"
+          :label="'profile-form.birth-date' | globalize"
+        />
+      </div>
+    </div>
+
+    <div class="app__form-row">
+      <div class="app__form-field">
+        <input-field
+          v-model="form.status"
+          name="profile-status"
+          :label="'profile-form.status' | globalize"
+        />
+      </div>
+    </div>
+
+    <div class="app__form-row">
+      <div class="app__form-field">
+        <input-field
+          v-model="form.avatarUrl"
+          name="profile-avatar-url"
+          :label="'profile-form.avatar-url' | globalize"
+        />
+      </div>
+    </div>
+
+    <div class="app__form-actions">
+      <button
+        type="submit"
+        class="profile-form__submit-btn app__button-primary"
+        :disabled="formMixin.isDisabled"
+      >
+        {{ 'profile-form.update-btn' | globalize }}
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+import FormMixin from '@/vue/mixins/form.mixin'
+
+import { vuexTypes } from '@/vuex'
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'profile-form',
+  mixins: [FormMixin],
+
+  data: _ => ({
+    form: {
+      name: '',
+      birthDate: '',
+      status: '',
+      avatarUrl: '',
+    },
+  }),
+
+  methods: {
+    ...mapActions({
+      signIn: vuexTypes.SIGN_IN,
+    }),
+
+    async submit () {
+      this.disableForm()
+      try {
+      } catch (e) {
+        alert(e.message)
+      }
+      this.enableForm()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import './app-form';
+</style>
--- src/vue/pages/profile.vue
+++ src/vue/pages/profile.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="profile">
-    Profile
+    <profile-form />
   </div>
 </template>
 
 <script>
+import ProfileForm from '@/vue/forms/profile-form'
+
 export default {
   name: 'profile',
+  components: { ProfileForm },
 }
 </script>
