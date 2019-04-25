d67c92ef2e99cd0844b755778c5f3ddc6ca7f7f0
ardier16 <ardier16@gmail.com>
2019-04-25 23:00:22 +0300

    Add feed displaying
    + use vue markdown component

--- src/vue/pages/feed.vue
+++ src/vue/pages/feed.vue
@@ -1,11 +1,50 @@
 <template>
   <div class="feed">
-    Feed
+    <div
+      v-for="post in posts"
+      :key="post._id"
+    >
+      <p>{{ post.title }}</p>
+      <vue-markdown :source="post.text" />
+      <p>Created: {{ post.createdAt | formatDate }}</p>
+      <p>Updated: {{ post.updatedAt | formatDate }}</p>
+      <hr>
+      <br>
+    </div>
   </div>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'
+
+import { vuexTypes } from '@/vuex'
+import { mapActions, mapGetters } from 'vuex'
+
 export default {
   name: 'feed',
+  components: {
+    VueMarkdown,
+  },
+
+  computed: {
+    ...mapGetters({
+      posts: vuexTypes.posts,
+    }),
+  },
+
+  async created () {
+    try {
+      await this.loadPosts()
+    } catch (e) {
+      console.error(e)
+      alert(e.message)
+    }
+  },
+
+  methods: {
+    ...mapActions({
+      loadPosts: vuexTypes.LOAD_POSTS,
+    }),
+  },
 }
 </script>
